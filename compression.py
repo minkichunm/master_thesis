1e6a97dc00563d2a26b94f303a155c4e17875cc9
minkichunm <minkichunm@gmail.com>
2024-01-22 00:23:02 +0100

    merge dev

---
+++ compression.py
@@ -0,0 +1,182 @@
+import tensorflow as tf
+import os
+from init_neuralnetwork import CompressibleNN
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow import keras
+from utils import *
+import pickle
+import json
+from Compressible_Huffman import Huffman
+import tensorflow.compat.v1 as tf1
+from regularization import quantize_weights
+
+regularization_loss_results = []
+ce_loss_results = []
+accuracy_results = []
+
+class CustomCallback(keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs=None):
+        regularization_loss_results.append(logs['regularization_loss'])
+        ce_loss_results.append(logs['loss_cross_entropy'])
+        accuracy_results.append(logs['accuracy'])  
+
+def compress_NN_param(options, x_train, y_train, x_test, y_test, train_generator, steps_per_epoch):
+    # load the saved weights
+    if options["load_model"]: 
+        print("Start compression with saved weights")
+    else:
+        print("Start compression")
+        
+    dataset_classes = {
+    "mnist": 10,
+    "cifar": 10,
+    "celeba": 2,
+    "3d": 2
+    }
+    	
+    # set parameters for training
+    coefficients = options["coefficients"]
+    num_epoch = options["epoch"]
+    batch_size = options["batch_size"]
+    regularization_type = options["regularization_type"]
+    directory = "results/" + options["directory_path"]
+    scale_outlier = options["scale_outlier"]
+    loss_results = []
+    num_class = dataset_classes[options["dataset"]]	
+    selected_model = options["model"]
+    val_accuracy_results = []
+    file_size_results = []
+    val_loss_results = []
+    compressibleNN_list = []
+  
+    # Check if the directory exists and create it if necessary
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    
+    # select model
+    selected_model = load_model_function(options["model"])
+    
+    # Create a list of models by copying the base model
+    for coeff in coefficients:
+        if options["dataset"] != '3d':
+            model_instance = CompressibleNN(selected_model(input_shape = x_train.shape[1:], num_classes = num_class), coeff, regularization_type, scale_outlier)
+        else:
+            model_instance = CompressibleNN(get_3d_model(),coeff, regularization_type, scale_outlier)
+        compressibleNN_list.append(model_instance)
+    
+    # Train the model with the current hyperparameters
+    #optimizer = tf.optimizers.Adam(learning_rate=1e-4, beta_1=0.5, beta_2=.95)
+    #optimizer = tf.optimizers.Adadelta(rho=0.9, epsilon=1e-7)
+    #optimizer = tf.optimizers.Adagard(learning_rate=1e-3, epsilon=1e-7)
+    optimizer = tf.optimizers.SGD(learning_rate=0.001)
+    
+    # Get the optimizer configuration as a dictionary
+    optimizer_config = optimizer.get_config()
+
+    # Convert the dictionary to a formatted string
+    optimizer_info = ", ".join(f"{key}={value}" for key, value in optimizer_config.items())
+
+    # Print the optimizer information in one line
+    print(f"Optimizer: {optimizer_info}")
+    
+    for count, compressibleNN in enumerate(compressibleNN_list):
+        # Create a learning rate scheduler
+        #lr_callback = tf.keras.callbacks.LearningRateScheduler(scheduler)
+        
+        compressibleNN.compile(optimizer,loss=keras.losses.SparseCategoricalCrossentropy(), metrics=['accuracy'])
+
+        # load the saved weights 
+        if options["load_model"]:
+            compressibleNN.load_weights(f'{directory}/model_{count}')
+        
+        checkpoint_callback = ModelCheckpoint(f'{directory}/model_{count}',
+                                      save_weights_only=True, mode='max',
+                                      monitor="val_accuracy",
+                                      save_best_only=True)
+             
+        history = compressibleNN.fit(train_generator, epochs=num_epoch, batch_size=batch_size, steps_per_epoch=steps_per_epoch,
+                                     validation_data=(x_test, y_test), callbacks=[CustomCallback(), checkpoint_callback])
+        
+        # Quantize weights after training
+        if options["post_quantization"]:
+            quantize_weights(compressibleNN.trainable_variables)
+        
+        celoss = history.history['loss_cross_entropy'][0]
+        regloss = history.history['regularization_loss'][0]
+    
+        val_accuracy_results.append(history.history['val_accuracy'])
+        val_loss_results.append(history.history['val_loss'])
+    
+        print(f"{compressibleNN.reg_type} {compressibleNN.regularization_coefficient} done")
+        #print(history.history['val_accuracy'])
+        #print(history.history.keys())
+        
+    # Define the full path to the log file
+    reg_filename = os.path.join(directory, "loss_logs.txt")
+    ce_filename = os.path.join(directory, "ce_logs.txt")
+    options_filename = os.path.join(directory, "options_logs.txt")
+    accuracy_filename = os.path.join(directory, "accuracy_logs.txt")
+    weights_size_filename = os.path.join(directory, "weights_size.txt")
+    val_accuracy_filename = os.path.join(directory, "val_accuracy_logs.txt")
+    val_loss_filename = os.path.join(directory, "val_loss_logs.txt")
+
+    # Save the original weights
+    for count, compressibleNN in enumerate(compressibleNN_list):
+        original_weights = compressibleNN.net_model.get_weights()
+        weights_filename = f'{directory}/original_model{count}_weights.pkl'
+
+        with open(weights_filename, 'wb') as file:
+            pickle.dump(original_weights, file)
+            
+
+    # Convert the TensorFlow tensors to normal Python floats and round to 3 decimal places
+    precision = 3
+    ce_loss_result = [round(float(ce_loss), precision) for ce_loss in ce_loss_results]
+    regularization_loss_result = [round(float(regularization_loss), precision) for regularization_loss in regularization_loss_results]
+    accuracy_result = [round(float(acc), precision) for acc in accuracy_results]
+    val_loss_result = [round(float(acc), precision) for sublist in val_loss_results for acc in sublist]
+    val_accuracy_result = [round(float(acc) * 100, precision) for sublist in val_accuracy_results for acc in sublist]
+
+    # Write about options
+    options_str = json.dumps(options)  # Convert the options dictionary to a JSON string
+    options_filename = os.path.join(directory, "options_logs.txt")
+    with open(options_filename, "a") as file:
+        file.write(options_str + "\n")
+        file.write(json.dumps(optimizer_config) + "\n")  # Convert and write optimizer_config as JSON
+
+    step = len(ce_loss_result) // len(coefficients)
+
+    # Write data about accuracy, validation, validation loss
+    write_data_to_file(os.path.join(directory, "accuracy_logs.txt"), accuracy_result, step)
+    write_data_to_file(os.path.join(directory, "val_accuracy_logs.txt"), val_accuracy_result, step)
+    write_data_to_file(os.path.join(directory, "val_loss_logs.txt"), val_loss_result, step)
+    write_data_to_file(os.path.join(directory, "loss_logs.txt"), regularization_loss_result, step)
+    write_data_to_file(os.path.join(directory, "ce_logs.txt"), ce_loss_result, step)
+
+    print("Save the CompressibleNN instance to a file for each model")
+    for count, compressibleNN in enumerate(compressibleNN_list):
+        trainedNN = Huffman(compressibleNN.net_model)
+        compressed_weights = trainedNN.compressNN()
+
+        # Save the CompressibleNN instance to a file for each compressibleNN
+        filename = f'{directory}/compressed_nn_{count}.pkl'  # Unique filename for each instance
+        with open(filename, 'wb') as outp:
+            pickle.dump(trainedNN.net_model.to_json(), outp, pickle.HIGHEST_PROTOCOL)
+            pickle.dump(trainedNN.codec, outp, pickle.HIGHEST_PROTOCOL)
+            pickle.dump(compressed_weights, outp, pickle.HIGHEST_PROTOCOL)
+
+        # Save the CompressibleNN instance to a file for each compressibleNN
+        weightfilename = f'{directory}/model{count}_compressed_weights.pkl'  # Unique filename for each instance
+        with open(weightfilename, 'wb') as outp:
+            pickle.dump(compressed_weights, outp, pickle.HIGHEST_PROTOCOL)
+
+        # Get the size of the saved file
+        file_size_bytes = os.path.getsize(weightfilename)
+        file_size_results.append(file_size_bytes)
+
+    write_to_file(os.path.join(directory, "weights_size.txt"), file_size_results)
+
+    print("Compression done")
+    
+    
---
+++ decompression.py
@@ -0,0 +1,88 @@
+import tensorflow as tf
+from tensorflow import keras
+import pickle
+from Compressible_Huffman import Huffman
+import os
+from utils import *
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+def decompress_NN_param(options, x_train, y_train, x_test, y_test, train_generator, steps_per_epoch):
+    print("Start decompression")
+
+    # Init
+    directory = "results/" + options["directory_path"]
+    cnt = 0
+    compNN_list = []
+    decompressed_weights_list = []
+
+    while True:
+        # Check if the file exists
+        filename = f'{directory}/compressed_nn_{cnt}.pkl'
+        if not os.path.exists(filename):
+            break
+
+        # Load the compressed model from the file
+        with open(filename, 'rb') as compressed_nn:
+            model_architecture = pickle.load(compressed_nn)
+            net_model = tf.keras.models.model_from_json(model_architecture)
+            codec = pickle.load(compressed_nn)
+            compressed_model_weights = pickle.load(compressed_nn)
+
+        # Create a new instance of CompressibleNN
+        compNN = Huffman(net_model)
+        compNN.codec = codec
+        compNN_list.append(compNN)
+        decompressed_weights = compNN.decompressNN(compressed_model_weights)
+        decompressed_weights_list.append(decompressed_weights)
+
+        # Increment the counter
+        cnt += 1
+
+    # Compare the original weights with the decompressed weights
+    original_model_weights_list = []
+    for cnt, compNN in enumerate(compNN_list):
+        # Check if the file exists
+        filename = f'{directory}/original_model{cnt}_weights.pkl'
+        if not os.path.exists(filename):
+            break
+        with open(filename, 'rb') as file:
+            original_model_weights = pickle.load(file)
+        original_model_weights_list.append(original_model_weights)
+
+        differences = compNN.compare_weights(original_model_weights_list[cnt], decompressed_weights_list[cnt])
+
+        # Print or log differences as needed
+        # print(f"Differences between original and decompressed weights in model{cnt}:")
+        # for i, diff in enumerate(differences):
+        #     print(f"Layer {i + 1}: Max Difference = {np.max(diff)}, Mean Difference = {np.mean(diff)}")
+        # print("-" * 80)
+
+    # Set decompressed weights
+    for compNN, decompressed_weights in zip(compNN_list, decompressed_weights_list):
+        compNN.set_weights(decompressed_weights)
+
+    # Reshape y_test to match the shape of predicted_labels using TensorFlow
+    y_test_flat = tf.cast(tf.squeeze(y_test), tf.int64)
+
+    acc_results = []
+
+    for cnt, compNN in enumerate(compNN_list):
+        # Make predictions using your model
+        predictions = compNN.predict(x_test)
+        # Calculate top-1 accuracy
+
+        predicted_labels = tf.argmax(predictions, axis=1)
+
+        top_1_accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted_labels, y_test_flat), tf.float32)) * 100
+
+        print(f"Model{cnt} Top-1 Accuracy: {top_1_accuracy:.2f}%")
+        acc_results.append(f"{top_1_accuracy:.2f}")
+    
+    print("Save accuracy log file")
+    # Define the full path to the log file
+    log_filename = os.path.join(directory, "accuracy.txt")
+
+    # Save the results to the file
+    write_to_file(log_filename, acc_results)
+        
+    print("Decompression done")
